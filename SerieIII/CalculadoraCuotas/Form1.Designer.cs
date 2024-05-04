6e0bc1ca6861204bf431ca5995b0cccc022c4fe2
mlyyppol <apenap1@miumg.edu.gt>
2024-05-04 12:51:05 -0600

    first commit

---
+++ SerieIII/CalculadoraCuotas/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CalculadoraCuotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.txtPlazo = new System.Windows.Forms.TextBox();
+            this.txtInteres = new System.Windows.Forms.TextBox();
+            this.txtResultadoCuotas = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtTotalIntereses = new System.Windows.Forms.TextBox();
+            this.txtTotalPagar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora de Cuotas";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Monto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Interes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rockwell Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Plazo";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtMonto.Font = new System.Drawing.Font("Rockwell Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMonto.Location = new System.Drawing.Point(102, 63);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(121, 30);
+            this.txtMonto.TabIndex = 4;
+            // 
+            // txtPlazo
+            // 
+            this.txtPlazo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtPlazo.Font = new System.Drawing.Font("Rockwell Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlazo.Location = new System.Drawing.Point(101, 188);
+            this.txtPlazo.Name = "txtPlazo";
+            this.txtPlazo.Size = new System.Drawing.Size(121, 32);
+            this.txtPlazo.TabIndex = 5;
+            // 
+            // txtInteres
+            // 
+            this.txtInteres.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtInteres.Font = new System.Drawing.Font("Rockwell Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInteres.Location = new System.Drawing.Point(102, 126);
+            this.txtInteres.Name = "txtInteres";
+            this.txtInteres.Size = new System.Drawing.Size(121, 32);
+            this.txtInteres.TabIndex = 6;
+            // 
+            // txtResultadoCuotas
+            // 
+            this.txtResultadoCuotas.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtResultadoCuotas.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultadoCuotas.Location = new System.Drawing.Point(21, 335);
+            this.txtResultadoCuotas.Name = "txtResultadoCuotas";
+            this.txtResultadoCuotas.Size = new System.Drawing.Size(225, 41);
+            this.txtResultadoCuotas.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCalcular.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCalcular.Location = new System.Drawing.Point(391, 126);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(148, 57);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtTotalIntereses
+            // 
+            this.txtTotalIntereses.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtTotalIntereses.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalIntereses.Location = new System.Drawing.Point(283, 335);
+            this.txtTotalIntereses.Name = "txtTotalIntereses";
+            this.txtTotalIntereses.Size = new System.Drawing.Size(225, 41);
+            this.txtTotalIntereses.TabIndex = 9;
+            // 
+            // txtTotalPagar
+            // 
+            this.txtTotalPagar.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtTotalPagar.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalPagar.Location = new System.Drawing.Point(545, 335);
+            this.txtTotalPagar.Name = "txtTotalPagar";
+            this.txtTotalPagar.Size = new System.Drawing.Size(225, 41);
+            this.txtTotalPagar.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(63, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Cuota Mensual";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(581, 303);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 18);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "total a pagar";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(271, 303);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(248, 18);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Total de intereses a pagar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTotalPagar);
+            this.Controls.Add(this.txtTotalIntereses);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtResultadoCuotas);
+            this.Controls.Add(this.txtInteres);
+            this.Controls.Add(this.txtPlazo);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.TextBox txtPlazo;
+        private System.Windows.Forms.TextBox txtInteres;
+        private System.Windows.Forms.TextBox txtResultadoCuotas;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtTotalIntereses;
+        private System.Windows.Forms.TextBox txtTotalPagar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SerieIII/CalculadoraCuotas/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraCuotas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double monto = double.Parse(txtMonto.Text);
+                double interes = double.Parse(txtInteres.Text);
+                double plazo = double.Parse(txtPlazo.Text);
+
+                double cuotaMensual = (monto * interes * Math.Pow(1 + interes, plazo)) / (Math.Pow(1 + interes, plazo) - 1);
+                cuotaMensual = Math.Round(cuotaMensual, 2);
+                txtResultadoCuotas.Text = cuotaMensual.ToString();
+
+                double totalIntereses = (cuotaMensual * plazo) - monto;
+                txtTotalIntereses.Text = totalIntereses.ToString();
+
+                double totalPagar = monto + totalIntereses;
+                txtTotalPagar.Text = totalPagar.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor ingrese valores válidos", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al calcular los valores: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
